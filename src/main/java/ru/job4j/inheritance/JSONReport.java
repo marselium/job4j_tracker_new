eba1d6fe748742f2c78e514f4490240c1b7bdded
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-04-06 22:01:33 +0300

    4. Переопределение метода [#174890 #373013]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        String sl = "\"";
+        return "{" + System.lineSeparator()
+                + "\t" + sl + "name" + sl + " : "
+                + sl + name + sl + ","+ System.lineSeparator()
+                + "\t" + sl + "body" + sl + " : "
+                + sl + body + sl + System.lineSeparator()
+                + "}";
+    }
+}
