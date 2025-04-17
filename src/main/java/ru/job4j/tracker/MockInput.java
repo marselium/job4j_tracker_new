5549516520ba5b2096f9d1651af7a49b6ce828c4
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-04-17 22:25:34 +0300

    8. Input и полиморфизм. [#181094 #380828]

--- src/main/java/ru/job4j/tracker/MockInput.java
+++ src/main/java/ru/job4j/tracker/MockInput.java
@@ -1,7 +1,12 @@
 package ru.job4j.tracker;
 
 public class MockInput implements Input {
+    private String[] answers;
+    private int position =0;
 
+    public MockInput(String[] answers) {
+        this.answers = answers;
+    }
     @Override
     public String askStr(String question) {
         return null;
